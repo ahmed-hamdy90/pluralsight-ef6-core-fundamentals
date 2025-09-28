e582c9254367b0e47a05e2cbccdf44aef34da273
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2025-09-28 20:12:01 +0300

    Complete the video called [Creating the Data Model with EF Core] under Chapter 02

---
+++ Ch02/03. Creating the Data Model with EF Core/PublisherConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// using PublisherDomain;
+
+Console.WriteLine("Hello, World!");
+
+// Testing load Domain Library
+// var book = new Book();
+// book.Title = "Testing";
+
+// Console.WriteLine(book.Title);
