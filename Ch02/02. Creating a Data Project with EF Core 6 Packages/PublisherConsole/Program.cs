548a66c687e816cb742b4d1d430f83b808a5aa12
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2025-09-28 20:11:10 +0300

    Complete the video called [Creating a Data Project with EF Core 6 Packages] under Chapter 02

---
+++ Ch02/02. Creating a Data Project with EF Core 6 Packages/PublisherConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+//// Testing load Domain Library
+// using PublisherDomain;
+// var book = new Book();
+// book.Title = "Testing";
+
+// Console.WriteLine(book.Title);
