43c0648785ab01b84a936914ab470d4a564c10d1
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2025-11-15 21:42:57 +0200

    Completed follow Steps for Video [Additional Filtering Capabilities] under Chapter 07

---
+++ Ch07/03. Additional Filtering Capabilities/PublisherData/PubContext.cs
@@ -0,0 +1,87 @@
+namespace PublisherData;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PublisherDomain;
+
+public class PubContext: DbContext
+{
+    public DbSet<Author> Authors { get; set; }
+
+    public DbSet<Book> Books { get; set; }
+
+    // Use Hard Code Configuration
+    // TODO: Replaced, AS now used for learning purpose 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var sqlConnectionString =
+            @"Server=tcp:mssql,1433;Database=PubDatabase;User Id=SA;Password=P@ass_LocalSql19;TrustServerCertificate=true;";
+        
+        // optionsBuilder
+        //     .UseSqlServer(sqlConnectionString) // First Add connection string
+        //     .LogTo(Console.WriteLine); // Define Logging SQL logs to Target, Here use Console.WriteLine as Delegate
+
+        /* We can Filter the Logs line through Pass Class Names as string **/
+        // optionsBuilder
+        //     .UseSqlServer(sqlConnectionString)
+        //     .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }); // Includes Command Execution Only
+        
+        /* We can Filter the Logs line through Pass Class Names as string Plus Define Logs Level Too **/
+        // optionsBuilder
+        //     .UseSqlServer(sqlConnectionString)
+        //     .LogTo(
+        //         Console.WriteLine,
+        //         new[] { DbLoggerCategory.Database.Command.Name },
+        //         LogLevel.Information); // Includes Command Execution Only Plus Information Log Level
+
+        /* We Need to Display Sensitive Data within Log's Output **/
+        optionsBuilder
+            .UseSqlServer(sqlConnectionString)
+            .LogTo(
+                Console.WriteLine,
+                new[] { DbLoggerCategory.Database.Command.Name },
+                LogLevel.Information)
+            .EnableSensitiveDataLogging();
+    }
+
+    // Use this next Method For Build our Seeding with EF Migration
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Use one Line Seed using HasData method
+        modelBuilder.Entity<Author>().HasData(
+            new Author { AuthorId = 1, FirstName = "Rhoda", LastName = "Lerman" });
+
+        // Prepare Array with Multi Author then use HasData method
+        var authorList = new Author[] {
+            new Author { AuthorId = 2, FirstName = "Ruth", LastName = "Ozeki" },
+            new Author { AuthorId = 3, FirstName = "Sofia", LastName = "Segovia" },
+            new Author { AuthorId = 4, FirstName = "Ursula K.", LastName = "LeGuin" },
+            new Author { AuthorId = 5, FirstName = "Hugh", LastName = "Howey" },
+            new Author { AuthorId = 6, FirstName = "Isabelle", LastName = "Allende" },
+        };
+        modelBuilder.Entity<Author>().HasData(authorList);
+
+        // Prepare Array with Multi Books then use HasData method
+        var someBooks = new Book[]
+        {
+            new Book { BookId = 1, AuthorId = 1, Title = "In God's Ear", PublishDate = new DateTime(1989, 3, 1) },
+            new Book { BookId = 2, AuthorId = 2, Title = "A Tale For the Time Being", PublishDate = new DateTime(2013, 12, 31) },
+            new Book { BookId = 3, AuthorId = 3, Title = "The Left Hand of Darkness", PublishDate = new DateTime(1969, 3, 1) }
+        };
+
+        modelBuilder.Entity<Book>().HasData(someBooks);
+
+        // Example of mapping UnConventional FK
+        // modelBuilder.Entity<Author>()
+        //             .HasMany(author => author.Books)
+        //             .WithOne(book => book.Author)
+        //             .HasForeignKey(book => book.AuthorFK);
+
+        // Option #2: To Enable of Adding Book without Author
+        // modelBuilder.Entity<Author>()
+        //     .HasMany(author => author.Books)
+        //     .WithOne(book => book.Author)
+        //     .HasForeignKey(book => book.AuthorId)
+        //     .IsRequired(false);
+    }
+}
